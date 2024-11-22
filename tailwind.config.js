68fb9943d5434194a564f71017b666c67acd7622
danendradipa <danendrach@gmail.com>
2024-11-23 01:01:07 +0700

    chore: Update Tailwind CSS configuration to include new file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./static/src/**/*.js"],
+  content: ["./templates/**/*.html", "./static/dist/**/*.js"],
   theme: {
     extend: {
       colors: {
-        primary: '#F48A18',
-        // secondary: ,
-      }, 
+        primary: "#EA580C",
+        secondary: "#F9FAFC",
+      },
       container: {
         center: true,
         padding: {
-          DEFAULT: '1rem',
-          sm: '1rem',
-          lg: '1rem',
-          xl: '1rem',
-          '2xl': '6rem',
+          DEFAULT: "1rem",
+          sm: "1rem",
+          lg: "1rem",
+          xl: "1rem",
+          "2xl": "6rem",
         },
       },
     },
