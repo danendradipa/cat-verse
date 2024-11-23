5ebe836cfcf663d885809ba113a5225612054a20
danendradipa <danendrach@gmail.com>
2024-11-23 17:04:22 +0700

    add swiper.js

---
+++ static/dist/js/swiper.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetween: 30,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
+  });
+});
